43570ff0b4adb35dd3ef29fbbf19c19247a3f319
CenterForDH <136561789+CenterForDH@users.noreply.github.com>
2023-09-11 11:34:01 +0900

    predict_sub.py

--- predict_sub.py
+++ predict_sub.py
@@ -48,7 +48,7 @@ st.markdown(str(footerText), unsafe_allow_html=True)
 @st.cache_data
 
 def model_file():
-    mfile = 'predict_substance_model.pkl'
+    mfile = '/mount/src/predict_substance/predict_substance_model.pkl'
     model = pickle.load(open(mfile, 'rb'))
 
     return model
